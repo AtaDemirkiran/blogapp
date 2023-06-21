98f46073eff4c7abd4cb07010962cd637d5373d2
= <atademirkiran@gmail.com>
2023-06-21 22:16:56 +0300

    Home page düzenleme

--- resources/views/frontend/default/index.blade.php
+++ resources/views/frontend/default/index.blade.php
@@ -10,12 +10,12 @@
             <div class="container px-4 px-lg-5 h-100">
                 <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
                     <div class="col-lg-8 align-self-end">
-                        <h1 class="text-white font-weight-bold">Your Favorite Place for Free Bootstrap Themes</h1>
+                        <h1 class="text-white font-weight-bold">Blog Websiteme Hoşgeldiniz.</h1>
                         <hr class="divider" />
                     </div>
                     <div class="col-lg-8 align-self-baseline">
-                        <p class="text-white-75 mb-5">Start Bootstrap can help you build better websites using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p>
-                        <a class="btn btn-primary btn-xl" href="#">Kategoriler</a>
+                        <p class="text-white-75 mb-5">Sizlere bilim, sağlık ,teknoloji ve daha farklı bir çok alan trendleri hakkında güncel bilgilere erişim sağlayan bir platform olarak hizmet veriyoruz. Trendleri takip etmek ve yenilikleri keşfetmek için bizi ziyaret edin!</p>
+                        <a class="btn btn-primary btn-xl" href="{{route('frontend.category.index')}}">Kategoriler</a>
                     </div>
                 </div>
             </div>
